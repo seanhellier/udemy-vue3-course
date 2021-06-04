a26cb7d278c94b611ee74708fc04ba2c1b781b92
Sean Leif Hellier <sean@newtropolis.com>
2021-06-04 18:05:46 -0400

    lesson 10

--- app.js
+++ app.js
@@ -1,15 +1,16 @@
 const app = Vue.createApp({
 	data() {
 		return {
+			showBooks: true,
 			title: "The Way of Kings",
 			author: "Brandon Sanderson",
 			age: 45,
 		};
 	},
 	methods: {
-		changeTitle(title) {
+		toggleShowBooks() {
 			// this.title = 'Words of Radiance'
-			this.title = title;
+			this.showBooks = !this.showBooks;
 		},
 	},
 });
