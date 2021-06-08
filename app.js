5974f8e7a3d4c93ff74e803028ed69590c00f64c
Sean Leif Hellier <sean@newtropolis.com>
2021-06-07 21:35:38 -0400

    lesson 13

--- app.js
+++ app.js
@@ -1,11 +1,24 @@
 const app = Vue.createApp({
 	data() {
 		return {
+			url: "https://www.thenetninja.co.uk",
 			showBooks: true,
 			books: [
-				{ title: "name of the wind", author: "patrick rothfuss" },
-				{ title: "the way of kings", author: "brandon sanderson" },
-				{ title: "the final empire", author: "brandon sanderson" },
+				{
+					title: "name of the wind",
+					author: "patrick rothfuss",
+					img: "assets/1.jpeg",
+				},
+				{
+					title: "the way of kings",
+					author: "brandon sanderson",
+					img: "assets/2.jpeg",
+				},
+				{
+					title: "the final empire",
+					author: "brandon sanderson",
+					img: "assets/3.jpeg",
+				},
 			],
 		};
 	},
