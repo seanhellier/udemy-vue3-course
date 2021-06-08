e73b852de85308c51dc92ad7c64127f353f9daf3
Sean Leif Hellier <sean@newtropolis.com>
2021-06-08 17:49:24 -0400

    lesson 14

--- app.js
+++ app.js
@@ -8,16 +8,19 @@ const app = Vue.createApp({
 					title: "name of the wind",
 					author: "patrick rothfuss",
 					img: "assets/1.jpeg",
+					isFav: true,
 				},
 				{
 					title: "the way of kings",
 					author: "brandon sanderson",
 					img: "assets/2.jpeg",
+					isFav: false,
 				},
 				{
 					title: "the final empire",
 					author: "brandon sanderson",
 					img: "assets/3.jpeg",
+					isFav: true,
 				},
 			],
 		};
